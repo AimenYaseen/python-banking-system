dc4f1442b5824e51412a987c73e99d659a19e240
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-06-26 14:05:00 +0500

    msg

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ from Bank import Bank
 if __name__ == '__main__':
     bank = Bank()
     
+# Banking System Requirements:
+# implements OOP concepts
+# Creates user account 
+# User can deposit, withdraw cash as well as transfer to other Users
+# User can view his/her account details
+# Maintain Transaction history
+
+# 1. Create Account
+# 2. Display Accounts
+# 3. Deposit Amount
+# 4. Withdraw Amount
+# 5. Transfer Amount
+# 6. Transaction History of specific account
+# 7. Transaction History
+# 8. Exit
